74ea11e6d0ca6a54c5f59bdd94fd1848f586ecba
michaellangford99 <michaellangford99@gmail.com>
2016-08-17 10:07:22 -0400

    removed temporary debug printout, no longer necessary

--- Source/tools/bintools.c
+++ Source/tools/bintools.c
@@ -3,13 +3,13 @@
 void set_bit(uint8_t bit, uint8_t* byte_to_set)
 {
   byte_to_set[0] |= (1 << bit);
-  printf("%d / 0x%x\n", byte_to_set[0], byte_to_set[0]);
+  //printf("%d / 0x%x\n", byte_to_set[0], byte_to_set[0]);
 }
 
 void clear_bit(uint8_t bit, uint8_t* byte_to_clear)
 {
   byte_to_clear[0] &= ~(1 << bit);
-  printf("%d / 0x%x\n", byte_to_clear[0], byte_to_clear[0]);
+  //printf("%d / 0x%x\n", byte_to_clear[0], byte_to_clear[0]);
 }
 
 int test_bit(uint8_t bit, uint8_t* byte_to_test)
