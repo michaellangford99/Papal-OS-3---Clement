7bec9fe7761cb8e79179a876c45060dfa3051bca
michaellangford99 <michaellangford99@gmail.com>
2016-08-17 10:50:46 -0400

    added comment on dynamic allocation of page directory

--- Source/mem/paging.c
+++ Source/mem/paging.c
@@ -12,6 +12,7 @@ int init_paging() {
   This basic paging code just identity maps the entire 4GB address space.
   */
   
+  //allocate 1KB for page directory
   page_directory = kmalloc(1024);
   
   printf("page_directory addr: 0x%x / %d / %d KB\n", (uint32_t)&page_directory, (uint32_t)&page_directory, ((uint32_t)&page_directory)/1024);
