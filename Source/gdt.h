6dad7511de1083f72c5508d8d0f40b64e3405bcc
michaellangford99 <michaellangford99@gmail.com>
2016-10-25 13:10:13 -0400

    added TSS struct

--- Source/gdt.h
+++ Source/gdt.h
@@ -16,6 +16,47 @@ struct GDT_sector_descriptor
 	uint8_t type;
 };
 
+struct {
+	int16_t	prev;
+	int16_t	reserved;
+	int32_t	esp0;
+	int16_t	ss0;
+	int16_t	reserved0;
+	int32_t	esp1;
+	int16_t	ss1;
+	int16_t	reserved1;
+	int32_t	esp2;
+	int16_t	ss2;
+	int16_t	reserved2;
+	int32_t	cr3;
+	int32_t	eip;
+	int32_t	eflags;
+	int32_t	eax;
+	int32_t	ecx;
+	int32_t	edx;
+	int32_t	ebx;
+	int32_t	esp;
+	int32_t	ebp;
+	int32_t	esi;
+	int32_t	edi;
+	int16_t	es;
+	int16_t	reserved3;
+	int16_t	cs;
+	int16_t	reserved4;
+	int16_t	ss;
+	int16_t	reserved5;
+	int16_t	ds;
+	int16_t	reserved6;
+	int16_t	fs;
+	int16_t	reserved7;
+	int16_t	gs;
+	int16_t	reserved8;
+	int16_t	ldt;
+	int16_t	reserved9;
+	int16_t	t;
+	int16_t	iomap;
+} __attribute__((packed)) TSS_descriptor;
+
 void gdt_init();
 void encode_GDT_entry(uint8_t *target, struct GDT_sector_descriptor source);
 extern void setGDT(uint32_t base, uint16_t size);
