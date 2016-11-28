2f97fb9c4aad55d8271fe373472fc9dbeb0422cd
michaellangford99 <michaellangford99@gmail.com>
2016-11-28 18:10:24 -0500

    made script create new c file and h file, also fills in H file with #define's

--- add-source.sh
+++ add-source.sh
@@ -1,5 +1,33 @@
+cd Source
 echo "creating c file $1"
 
-echo -n '#include "' >> Source/headers.h
-echo -n $1 >> Source/headers.h
-echo '.h"' >> Source/headers.h
+#if necessary, create directory
+if [[ $1 == */* ]] ; then
+	DIR=${1%/*}
+	echo $DIR 
+	mkdir $DIR
+fi
+
+#add new c file to headers list
+echo -n '#include "' >> headers.h
+echo -n $1 >> headers.h
+echo '.h"' >> headers.h
+
+#create c file
+CFILE="${1}.c"
+if [[ $CFILE == */* ]] ; then
+        echo '#include "../system.h"' >> $CFILE
+else
+	echo '#include "system.h"' >> $CFILE
+fi
+#create h file
+HFILE="${1}.h"
+HIF=`echo "$1" | tr '[:lower:]' '[:upper:]'`
+HIF="${HIF#*/}"
+HIF="${HIF}_H"
+echo "#ifndef $HIF" >> $HFILE
+echo "#define $HIF" >> $HFILE
+echo "" >> $HFILE
+echo "#endif" >> $HFILE
+
+cd ..
