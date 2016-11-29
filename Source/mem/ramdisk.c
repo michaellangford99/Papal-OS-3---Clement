8cb1f43f51164e206fc1120c8ea63b858e6c4578
michaellangford99 <michaellangford99@gmail.com>
2016-11-29 12:51:11 -0500

    added debug dump of modules

--- Source/mem/ramdisk.c
+++ Source/mem/ramdisk.c
@@ -1,2 +1,28 @@
 #include "../system.h"
 
+int load_ramdisk(struct multiboot_header* mboot_header)
+{
+  //later we will also parse the ramdisk file (if there is one in the module list)
+  printf(" ------------Loading INITRD image & ramdisk --------------\n\n");
+  
+  if (mboot_header->mods_count > 0)
+  {
+    for (int i = 0; i < (int)mboot_header->mods_count; i++)
+    {
+    	mboot_mod_desc_t* mod_info = (mboot_mod_desc_t*)(mboot_header->mods_addr + (i*sizeof(mboot_mod_desc_t)));
+      
+      printf("module %d start : %d, '%s'\n", i, mod_info->start, mod_info->start);
+      printf("module %d end : %d, '%s'\n", i, mod_info->end, mod_info->end);
+      printf("module %d string : %d, '%s'\n", i, mod_info->string, mod_info->string);
+      printf("\n");
+    }
+  }
+  else
+  {
+    printf("-no modules found, and no initrd image-\n");
+  }
+  
+  
+  
+  return K_SUCCESS;
+}
--- Source/mem/ramdisk.h
+++ Source/mem/ramdisk.h
@@ -1,4 +1,17 @@
 #ifndef RAMDISK_H
 #define RAMDISK_H
 
+typedef struct {
+  uint32_t* base_ptr;
+  uint32_t root_entries;
+} __attribute__((packed)) ramdisk_descriptor;
+
+typedef struct {
+  uint32_t type;
+  uint32_t size;
+  /* more coming */
+} __attribute__((packed)) ramdisk_file_descriptor;
+
+int load_ramdisk(struct multiboot_header* mboot_header);
+
 #endif
