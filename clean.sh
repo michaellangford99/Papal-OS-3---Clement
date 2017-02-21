67ddcf2e8ae9b35b99c6d5b8e53343564ea736a8
michaellangford99 <michaellangford99@gmail.com>
2017-02-21 10:36:10 -0500

    updated clean script to remove copies of headers

--- clean.sh
+++ clean.sh
@@ -2,3 +2,37 @@ rm Bin/*.o
 rm Bin/*.bin
 rm Build/*.iso
 rm Grub/boot/clement.bin
+
+
+cd sysroot/usr/include
+
+SRC_DIRS=`ls -d */ | sed 's#/##'`
+SRC_DIRS=". $SRC_DIRS"
+
+HFILES=""
+
+#create list of all h files
+for dir in $SRC_DIRS
+do
+	for hfile in `ls $dir/*.h`
+	do
+		HFILES="$HFILES $hfile"
+	done
+done
+
+echo $HFILES
+
+for file in $HFILES
+do
+  echo "removing $file"
+  rm $file
+done
+
+
+
+SRC_DIRS=`ls -d */ | sed 's#/##'`
+for dir in $SRC_DIRS
+do
+  echo $dir
+	rm -d $dir
+done
