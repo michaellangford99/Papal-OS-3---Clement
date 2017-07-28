787b0547be9a6494c238153ab6d69ae0fc90c1f6
michaellangford99 <michaellangford99@gmail.com>
2017-07-28 07:55:24 -0400

    Completed logic for heap allocation, helper functions still must be added

--- Source/mem/heap.c
+++ Source/mem/heap.c
@@ -12,6 +12,7 @@ heap_node_t* heap_list;
 int heap_init()
 {
   printf("size of heap node is %d bytes\n", sizeof(heap_node_t));
+  printf("dta+bitmap: %d", HEAP_CHUNK_HEADER_SIZE+HEAP_CHUNK_BITMAP_SIZE+HEAP_CHUNK_DATA_SIZE);
   heap_list = (heap_node_t*)kmalloc(sizeof(heap_node_t));
   heap_list->next = NULL;
   
@@ -27,11 +28,11 @@ uint32_t* heap_alloc(uint32_t length)
   heap_node_t* node = heap_list;
   
   bool found_free_node = false;
-  while(node->next != NULL)
+  while(node->next != NULL) //loop through nodes
   {
-    if (node->memory_allocated < HEAP_CHUNK_DATA_SIZE)
+    if (node->memory_allocated < HEAP_CHUNK_DATA_SIZE) //check if there is any data left
     {
-      if (HEAP_CHUNK_DATA_SIZE - node->memory_allocated > length)
+      if ((HEAP_CHUNK_DATA_SIZE - node->memory_allocated) > length) //check if we can fit it
       {
         bool success = false;
         uint32_t return_value = heap_allocate_from_node(node, length, &success);
@@ -48,9 +49,38 @@ uint32_t* heap_alloc(uint32_t length)
   return NULL;
 }
 
-uint32_t heap_allocate_from_node(heap_node_t* node, int length, bool* success)
+uint32_t heap_allocate_from_node(heap_node_t* node, uint32_t length, bool* success)
 {
-  *success = true;
+  for (uint32_t bit = 0; bit < HEAP_CHUNK_DATA_SIZE-1; bit++)//loop through each bit in the bitmap
+  {
+    if (test_bit(bit%8, &(node->heap_chunk_bitmap[bit/8])))     //if a free on is found, loop for count
+    {
+      bool completed_loop = true;
+      for (uint32_t i = 0; i < length; i++)
+      {
+        if (!test_bit((bit+i)%8, &(node->heap_chunk_bitmap[(bit+i)/8])))
+        {
+          completed_loop = false;
+          break;
+        }
+      }
+      //if it got through all the bits succesfully, 'completed_loop' = true
+      if (completed_loop)
+      {
+        mark_bits(&(node->heap_chunk_bitmap[bit/8]), length);
+        
+      }
+      else//else, increase 'bit' by the amount we just traversed (becase we already checked it) 
+      {
+        
+      }
+    }
+  }
+  return 0;
+}
+
+uint32_t mark_bits(uint8_t* bitmap_address, uint32_t bit_count)
+{
   return 0;
 }
 
--- Source/mem/heap.h
+++ Source/mem/heap.h
@@ -1,8 +1,8 @@
 #ifndef HEAP_H
 #define HEAP_H
 
-#define HEAP_CHUNK_SIZE_1       256
-#define HEAP_CHUNK_SIZE_2       64
+/*#define HEAP_CHUNK_SIZE_1       256
+#define HEAP_CHUNK_SIZE_2       64*/
 
 #define HEAP_CHUNK_HEADER_SIZE  8 //space for pointer to next struct, and integer for amount of space left in chunk
 #define HEAP_CHUNK_DATA_SIZE    (((4096-HEAP_CHUNK_HEADER_SIZE)*8)/9)
@@ -11,14 +11,15 @@
 typedef struct heap_node {
   uint32_t memory_allocated;
   struct heap_node* next;
-  //after these 8 bytes, the rest is the bitap and data
+  //after the previous 8 bytes, the rest is the bitap and data
   uint8_t heap_chunk_bitmap[HEAP_CHUNK_BITMAP_SIZE];
   //data....  
 }  __attribute__((packed)) heap_node_t;//must be packed, in order to fit in 4096 bytes
 
 int heap_init();
 uint32_t* heap_alloc(uint32_t length);
-uint32_t heap_allocate_from_node(heap_node_t* node, int length, bool* success);
+uint32_t heap_allocate_from_node(heap_node_t* node, uint32_t length, bool* success);
 int heap_free(uint32_t* addr, uint32_t length);
+uint32_t mark_bits(uint8_t* bitmap_address, uint32_t bit_count);
 
 #endif
