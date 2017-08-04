95417a00252e2e97a97c07507dcefadaae25832a
michaellangford99 <michaellangford99@gmail.com>
2017-08-04 04:09:30 -0400

    fixed erroneous type name in header

--- Source/cpu/x86.h
+++ Source/cpu/x86.h
@@ -3,10 +3,10 @@
 
 struct x86_registers
 {
-  uuint32_t gs, fs, es, ds;
-  uuint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
-  uuint32_t int_no, err_code;
-  uuint32_t eip, cs, eflags, useresp, ss;
+  uint32_t gs, fs, es, ds;
+  uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+  uint32_t int_no, err_code;
+  uint32_t eip, cs, eflags, useresp, ss;
 };
 
 struct x86_tss {
