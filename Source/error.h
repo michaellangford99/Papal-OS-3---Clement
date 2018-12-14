91a51bfb02730291e0f771b1158ad749b051b786
michaellangford99 <michaellangford99@gmail.com>
2018-12-14 08:00:51 -0500

    Changed error constants

--- Source/error.h
+++ Source/error.h
@@ -1,12 +1,12 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-#define K_FATAL          10
+#define K_FATAL          -1
 #define K_ERROR_M        0
 #define K_SUCCESS        1
 
-#define USR_FATAL        9
+#define USR_FATAL        -1
 #define USR_EXIT         1
-#define USR_EXIT_ERROR   2
+#define USR_EXIT_ERROR   0
 
 #endif
