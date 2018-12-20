fe54834ae37fba93bc6f5389ea86228119546054
Michael Langford <michaellangford99@gmail.com>
2018-12-19 22:30:37 -0500

    removed unused functions for entering user mode

--- Source/cpu/gdt.asm
+++ Source/cpu/gdt.asm
@@ -30,48 +30,6 @@ setTSS:
   ltr ax
   ret
 
-;uint32_t eip, cs, eflags, useresp, ss;
-extern save_ring_0_esp
-
-global run_user_mode
-run_user_mode:
-
-  ;push 0xdeadbeef
-  ;int 1
-
-  mov eax, esp
-  push eax
-  call save_ring_0_esp
-
-  ;switch segment data registers
-  mov ax, 0x23
-  mov  ds, ax
-  mov  es, ax
-  mov  fs, ax
-  mov  gs, ax
-  ;  ss, ax
-
-  ;save esp
-  mov eax, esp
-
-  ;push ss
-  push 0x23
-
-  ;push esp
-  push eax
-
-  ;push flags
-  ;cli
-  pushf
-
-  ;push cs
-  push 0x1B
-
-  ;push eip
-  push user_mode_function2
-
-  iret
-
 global user_mode_function
 user_mode_function:
 
